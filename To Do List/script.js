16bd81d4793b551a5ada549ca5e3b8587feb6a73
Mahansa <hansagamers11@gmail.com>
2025-05-10 13:23:30 +0700

    Add mini localStorage

--- To Do List/script.js
+++ To Do List/script.js
@@ -13,9 +13,16 @@ function Tambah() {
     list.appendChild(listItem);
 
     document.getElementById("inputText").value = "";
+    localStorage.setItem(i,list)
+    
+    let local = localStorage.getItem(i,list)
+    
 }
 
 function Hapus() {
     let list = document.getElementById("list");
     list.innerHTML = "";
 }
+
+
+   
