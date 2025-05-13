f285074d26cb82216ff9d2a0409cdbea805678d7
Mahansa <hansagamers11@gmail.com>
2025-05-13 11:32:20 +0700

    Update delete one on one

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,3 +1,4 @@
+
 function Tambah() {
     let i = document.getElementById("inputText").value;
 
@@ -9,26 +10,34 @@ function Tambah() {
     let listItem = document.createElement('li');
     listItem.innerText = i;
 
+  
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = "Hapus";
+    deleteBtn.style.marginLeft = "10px";
+    deleteBtn.onclick = function () {
+        listItem.remove();
+        updateLocalStorage();
+    };
+
+    listItem.appendChild(deleteBtn);
+
     let list = document.getElementById("list");
     list.appendChild(listItem);
 
     document.getElementById("inputText").value = "";
-    
-    let currentList = Array.from(list.children).map(li => li.textContent);
-    localStorage.setItem("todolist", JSON.stringify(currentList));
-    
+    updateLocalStorage();
 }
 
 function Hapus() {
-          
-    let list = document.getElementById("list");
-    while (list.firstChild) {
-        list.removeChild(list.firstChild);
-    }
+    document.getElementById("list").innerHTML = "";
     localStorage.removeItem("todolist");
-    document.getElementById("inputText").value = "";
 }
 
+function updateLocalStorage() {
+    let list = document.getElementById("list");
+    let currentList = Array.from(list.children).map(li => li.childNodes[0].textContent.trim());
+    localStorage.setItem("todolist", JSON.stringify(currentList));
+}
 
 window.onload = function() {
     let savedList = JSON.parse(localStorage.getItem("todolist"));
@@ -37,6 +46,16 @@ window.onload = function() {
         savedList.forEach(item => {
             let listItem = document.createElement('li');
             listItem.innerText = item;
+
+            let deleteBtn = document.createElement('button');
+            deleteBtn.innerText = "Hapus";
+            deleteBtn.style.marginLeft = "10px";
+            deleteBtn.onclick = function () {
+                listItem.remove();
+                updateLocalStorage();
+            };
+
+            listItem.appendChild(deleteBtn);
             list.appendChild(listItem);
         });
     }
